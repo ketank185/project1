afcdfcdbc687b41a2910059ce320f5cc0b67d0e2
Ketan Kadkuntwar <121004840+ketank185@users.noreply.github.com>
2023-03-15 16:48:17 +0530

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,4 +1,5 @@
-mysql -h database-1.cayvcljjcn2k.ap-south-1.rds.amazonaws.com --user=admin --password=admin1235 <<EOF   
+mysql -h database-1.cayvcljjcn2k.ap-south-1.rds.amazonaws.com --user=admin --password=admin1235 << EOF
+use demo;
 CREATE TABLE `USER` ( `id` int(10) unsigned NOT NULL auto_increment,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
@@ -7,4 +8,5 @@ CREATE TABLE `USER` ( `id` int(10) unsigned NOT NULL auto_increment,
   `password` varchar(45) NOT NULL,
   `regdate` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1; EOF
+) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+EOF

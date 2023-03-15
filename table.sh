08e06ae9bfc12c877862f66003cb55e14dec2112
Ketan Kadkuntwar <121004840+ketank185@users.noreply.github.com>
2023-03-15 22:45:03 +0530

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,5 +1,3 @@
-cat <<EOF | mysql -h database-1.cayvcljjcn2k.ap-south-1.rds.amazonaws.com --user=admin --password=admin1235
-use demo;
 CREATE TABLE `USER` ( `id` int(10) unsigned NOT NULL auto_increment,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
@@ -8,5 +6,4 @@ CREATE TABLE `USER` ( `id` int(10) unsigned NOT NULL auto_increment,
   `password` varchar(45) NOT NULL,
   `regdate` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
-EOF
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
